c7f6808f1fdc4290b593b277eda14c9b7d0e4d03
IPO0123069 <IPO0123069@CL227-14>
2024-04-13 09:02:49 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	setlocale(0, "rus");
+
+	int tc, tf, res_tf;
+	cout << " введите градус по цельсию \n ";
+	cin >> tc;
+	tf = (tc * 9 / 5) + 32;
+	std::cout << tf;
+}
